f3763e5561c365736cd316a1a920016b25200980
Aiden <bokjaeoh@gmail.com>
2024-11-06 19:28:32 +0900

    Add a module that creates and returns a `React.ReactElement`(JSX element)

---
+++ src/core.jsx
@@ -0,0 +1,17 @@
+/**
+ * Returns after creating a `React.ReactElement`(JSX element).
+ *
+ * @module createElement
+ * @param {React.ComponentProps} [props] `React.ComponentProps` passed to React functional components.
+ * @param {React.ReactNode} [props.children] `React.ReactNode` to include at the `React.ReactElement` that will be created.
+ * @param {React.ComponentProps} [props.props] `React.ComponentProps` to apply to the `React.ReactElement` that will be created.
+ * @param {String} [props.tag="div"] HTML element name to apply to the `React.ReactElement` that will be created.
+ * @returns {React.ReactElement} Returns the created `React.ReactElement` (JSX element).
+ */
+const createElement = ({ children, props, tag }) => {
+  const Tag = tag ? tag : "div"
+
+  return <Tag {...props}>{children}</Tag>
+}
+
+export { createElement }
