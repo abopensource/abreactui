6b8ddb769ab3e464843a04ffbc9285a1fa4754c4
Aiden <bokjaeoh@gmail.com>
2024-11-06 18:39:38 +0900

    Change component directory structure

--- samples/src/index.jsx
+++ samples/src/index.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 
 import "./index.css"
-import App from "./App"
+import { App } from "./components"
 import { log, reportWebVitals } from "./utils"
 
 const nodeEn = process.env.NODE_ENV || "production"
