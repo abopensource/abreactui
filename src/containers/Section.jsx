48b72cb1be710e01c2cb9c05e0b66af4e353302b
Aiden <bokjaeoh@gmail.com>
2024-11-21 20:40:08 +0900

    Update `Container`, `Section` component and stylesheet module

--- src/containers/Section.jsx
+++ src/containers/Section.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types"
 import React from "react"
 
-import { createElement, styleSection } from "../"
+import { createElement, style } from "../"
 
 /**
  * Returns after created a section element(JSX element).
@@ -12,17 +12,17 @@ import { createElement, styleSection } from "../"
  * @param {import("react").ReactNode} [props.children] Child nodes to include in the section element to be created.
  * @param {String} [props.className] Stylesheet class name to apply to the section element to be created.
  * @param {String} [props.tag="section"] HTML tag to use for the section element to be created.
- * @param {import("react").ForwardedRef} [ref] Object or function for use by referencing a component that will be created from a parent component.
+ * @param {import("react").ForwardedRef} [forwardedRef] Object or function for use by referencing a component that will be created from a parent component.
  * @returns {import("react").ReactElement} Returns the created section element(JSX element).
  */
-const Section = React.forwardRef(function Section(props, ref) {
+const Section = React.forwardRef(function Section(props, forwardedRef) {
   const { children, className, tag, ...propsOther } = props
 
-  const styles = [styleSection.Section]
+  const styles = [style.Section]
   className && styles.push(className)
 
   const propsSection = { ...propsOther, className: styles.join(" ") }
-  ref && (propsSection.ref = ref)
+  forwardedRef && (propsSection.ref = forwardedRef)
 
   return createElement({
     children,
@@ -30,6 +30,7 @@ const Section = React.forwardRef(function Section(props, ref) {
     tag: tag ? tag : "section",
   })
 })
+Section.displayName = "Section"
 Section.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,15 +1,6 @@
 import style from "./Styles.module.css"
 import styleButton from "./Button.module.css"
-import styleContainer from "./Container.module.css"
 import styleInput from "./Input.module.css"
-import styleSection from "./Section.module.css"
 import styleText from "./Text.module.css"
 
-export {
-  style,
-  styleButton,
-  styleContainer,
-  styleInput,
-  styleSection,
-  styleText,
-}
+export { style, styleButton, styleInput, styleText }
