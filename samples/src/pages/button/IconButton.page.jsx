7afa37199e081867c66eb33b8e6cb6f8b64d48b4
Aiden <bokjaeoh@gmail.com>
2024-11-22 17:28:37 +0900

    Update buttons component sample pages

--- samples/src/pages/button/IconButton.page.jsx
+++ samples/src/pages/button/IconButton.page.jsx
@@ -1,4 +1,4 @@
-import { IconButton } from "abreactui"
+import { Box, IconButton } from "abreactui"
 import { useRef } from "react"
 import {
   BsBellFill,
@@ -17,7 +17,7 @@ import "../styles"
  * @type {import("react").ReactElement}
  */
 const IconButtonPage = () => {
-  const myButton = useRef(null)
+  const refButton = useRef(null)
 
   return (
     <div className="Pages SamplePage IconButtonPage">
@@ -29,307 +29,431 @@ const IconButtonPage = () => {
       <section className="Samples">
         <article className="Sample">
           <h2>Basic</h2>
-          {/* prettier-ignore */}
-          <pre>
-            <code>
-                {
-`<IconButton>
-  <BsBellFill />
-</IconButton>
-<IconButton>
-  <BsBookmarkFill />
-</IconButton>
-<IconButton>
-  <BsGrid />
-</IconButton>
-<IconButton>
-  <BsTrash3 />
-</IconButton>`
-                }
+          <div className="Example">
+            <pre>
+              <code>
+                {`import { IconButton } from "abreactui"
+import { BsBellFill, BsBookmarkFill, BsGrid, BsTrash3 } from "react-icons/bs"
+
+export const IconButtonSample = () => {
+  return (
+    <>
+      <IconButton>
+        <BsBellFill />
+      </IconButton>
+      <IconButton>
+        <BsBookmarkFill />
+      </IconButton>
+      <IconButton>
+        <BsGrid />
+      </IconButton>
+      <IconButton>
+        <BsTrash3 />
+      </IconButton>
+    </>
+  )
+}`}
               </code>
-          </pre>
-          <div className="Result">
-            <IconButton>
-              <BsBellFill />
-            </IconButton>
-            <IconButton>
-              <BsBookmarkFill />
-            </IconButton>
-            <IconButton>
-              <BsGrid />
-            </IconButton>
-            <IconButton>
-              <BsTrash3 />
-            </IconButton>
+            </pre>
+            <div className="Result">
+              <IconButton>
+                <BsBellFill />
+              </IconButton>
+              <IconButton>
+                <BsBookmarkFill />
+              </IconButton>
+              <IconButton>
+                <BsGrid />
+              </IconButton>
+              <IconButton>
+                <BsTrash3 />
+              </IconButton>
+            </div>
           </div>
         </article>
         <article className="Sample">
           <h2>
             <code>className</code> props
           </h2>
-          {/* prettier-ignore */}
-          <pre>
-            <code>
-              {
-`<IconButton className="myButton">
-  <BsBellFill />
-</IconButton>`
-              }
-            </code>
-          </pre>
-          <div className="Result">
-            <IconButton className="myButton">
-              <BsBellFill />
-            </IconButton>
+          <div className="Example">
+            <pre>
+              <code>
+                {`import { IconButton } from "abreactui"
+import { BsBellFill } from "react-icons/bs"
+
+export const IconButtonSample = () => {
+  return (
+    <>
+      <style>{".myButton { border: 1px solid yellowgreen; }"}</style>
+      <IconButton className="myButton">
+        <BsBellFill />
+      </IconButton>
+    </>
+  )
+}`}
+              </code>
+            </pre>
+            <div className="Result">
+              <style>{".myButton { border: 1px solid yellowgreen; }"}</style>
+              <IconButton className="myButton">
+                <BsBellFill />
+              </IconButton>
+            </div>
           </div>
         </article>
         <article className="Sample">
           <h2>
             <code>disabled</code> props
           </h2>
-          {/* prettier-ignore */}
-          <pre>
-            <code>
-              {
-`<IconButton disabled>
-  <BsBellFill />
-</IconButton>
-<IconButton disabled>
-  <BsBookmarkFill />
-</IconButton>
-<IconButton disabled>
-  <BsGrid />
-</IconButton>
-<IconButton disabled>
-  <BsTrash3 />
-</IconButton>`
-              }
-            </code>
-          </pre>
-          <div className="Result">
-            <IconButton disabled>
-              <BsBellFill />
-            </IconButton>
-            <IconButton disabled>
-              <BsBookmarkFill />
-            </IconButton>
-            <IconButton disabled>
-              <BsGrid />
-            </IconButton>
-            <IconButton disabled>
-              <BsTrash3 />
-            </IconButton>
+          <div className="Example">
+            <pre>
+              <code>
+                {`import { IconButton } from "abreactui"
+import { BsBellFill, BsBookmarkFill, BsGrid, BsTrash3 } from "react-icons/bs"
+
+export const IconButtonSample = () => {
+  return (
+    <>
+      <IconButton disabled>
+        <BsBellFill />
+      </IconButton>
+      <IconButton disabled>
+        <BsBookmarkFill />
+      </IconButton>
+      <IconButton disabled>
+        <BsGrid />
+      </IconButton>
+      <IconButton disabled>
+        <BsTrash3 />
+      </IconButton>
+    </>
+  )
+}`}
+              </code>
+            </pre>
+            <div className="Result">
+              <IconButton disabled>
+                <BsBellFill />
+              </IconButton>
+              <IconButton disabled>
+                <BsBookmarkFill />
+              </IconButton>
+              <IconButton disabled>
+                <BsGrid />
+              </IconButton>
+              <IconButton disabled>
+                <BsTrash3 />
+              </IconButton>
+            </div>
           </div>
         </article>
         <article className="Sample">
           <h2>
-            <code>style</code> props
+            <code>href</code> props
           </h2>
-          {/* prettier-ignore */}
-          <pre>
-            <code>
-              {
-`<IconButton style={{ color: "yellowgreen" }}>
-  <BsBellFill />
-</IconButton>
-<IconButton style={{ color: "red" }}>
-  <BsTrash3 />
-</IconButton>`
-              }
-            </code>
-          </pre>
-          <div className="Result">
-            <IconButton style={{ color: "yellowgreen" }}>
-              <BsBellFill />
-            </IconButton>
-            <IconButton style={{ color: "red" }}>
-              <BsTrash3 />
-            </IconButton>
-          </div>
-        </article>
-        <article className="Sample">
-          <h2>
-            <code>styled</code> props
-          </h2>
-          {/* prettier-ignore */}
-          <pre>
-            <code>
-              {
-`<IconButton styled="fill">
-  <BsBellFill />
-</IconButton>
-<IconButton styled="fill">
-  <BsTrash3 />
-</IconButton>
-<IconButton styled="outline">
-  <BsBellFill />
-</IconButton>
-<IconButton styled="outline">
-  <BsTrash3 />
-</IconButton>`
-              }
-            </code>
-          </pre>
-          <div className="Result">
-            <IconButton styled="fill">
-              <BsBellFill />
-            </IconButton>
-            <IconButton styled="fill">
-              <BsTrash3 />
-            </IconButton>
-            <IconButton styled="outline">
-              <BsBellFill />
-            </IconButton>
-            <IconButton styled="outline">
-              <BsTrash3 />
-            </IconButton>
-          </div>
-          {/* prettier-ignore */}
-          <pre>
-            <code>
-              {
-`Underline button component sample: &nbsp;
-<IconButton>
-  <BsBellFill />
-</IconButton>
-<IconButton>
-  <BsTrash3 />
-</IconButton>
-<IconButton styled="normal">
-  <BsBookmarkFill />
-</IconButton>
-<IconButton styled="normal">
-  <BsGrid />
-</IconButton>`
-              }
-            </code>
-          </pre>
-          <div className="Result">
-            Normal button component sample: &nbsp;
-            <IconButton>
-              <BsBellFill />
-            </IconButton>
-            <IconButton>
-              <BsTrash3 />
-            </IconButton>
-            <IconButton styled="normal">
-              <BsBookmarkFill />
-            </IconButton>
-            <IconButton styled="normal">
-              <BsGrid />
-            </IconButton>
-          </div>
-        </article>
-        <article className="Sample">
-          <h2>
-            <code>styled disabled</code> props
-          </h2>
-          {/* prettier-ignore */}
-          <pre>
-            <code>
-              {
-`<IconButton disabled styled="fill">
-  <BsBellFill />
-</IconButton>
-<IconButton disabled styled="outline">
-  <BsTrash3 />
-</IconButton>`
-              }
-            </code>
-          </pre>
-          <div className="Result">
-            <IconButton disabled styled="fill">
-              <BsBellFill />
-            </IconButton>
-            <IconButton disabled styled="outline">
-              <BsTrash3 />
-            </IconButton>
-          </div>
-          {/* prettier-ignore */}
-          <pre>
-            <code>
-              {
-`Underline button component sample: &nbsp;
-<IconButton disabled>
-  <BsBookmarkFill />
-</IconButton>
-<IconButton disabled styled="normal">
-  <BsGrid />
-</IconButton>`
-              }
-            </code>
-          </pre>
-          <div className="Result">
-            Underline button component sample: &nbsp;
-            <IconButton disabled>
-              <BsBookmarkFill />
-            </IconButton>
-            <IconButton disabled styled="normal">
-              <BsGrid />
-            </IconButton>
-          </div>
-        </article>
-        <article className="Sample">
-          <h2>
-            <code>tag</code> props
-          </h2>
-          {/* prettier-ignore */}
-          <pre>
-            <code>
-              {
-`<IconButton tag="a" href="https://google.com" target="blank">
-  <BsGoogle />
-</IconButton>`
-              }
-            </code>
-          </pre>
-          <div className="Result">
-            <IconButton tag="a" href="https://google.com" target="blank">
-              <BsGoogle />
-            </IconButton>
-          </div>
-          {/* prettier-ignore */}
-          <pre>
-            <code>
-              {
-`<IconButton
-  href="https://google.com"
-  styled="outline"
-  target="blank"
->
-  <BsGoogle />
-</IconButton>`
-              }
-            </code>
-          </pre>
-          <div className="Result">
-            <IconButton
-              href="https://google.com"
-              styled="outline"
-              target="blank"
-            >
-              <BsGoogle />
-            </IconButton>
+          <div className="Example">
+            <pre>
+              <code>
+                {`import { IconButton } from "abreactui"
+import { BsGoogle } from "react-icons/bs"
+
+export const IconButtonSample = () => {
+  return (
+    <IconButton href="https://google.com" target="blank">
+      <BsGoogle />
+    </IconButton>
+  )
+}`}
+              </code>
+            </pre>
+            <div className="Result">
+              <IconButton href="https://google.com" target="blank">
+                <BsGoogle />
+              </IconButton>
+            </div>
           </div>
         </article>
         <article className="Sample">
           <h2>
             <code>ref</code> props
           </h2>
-          {/* prettier-ignore */}
-          <pre>
-            <code>
-              {
-`<IconButton ref={myButton}>
-  <BsBellFill />
-</IconButton>`
-              }
-            </code>
-          </pre>
-          <div className="Result">
-            <IconButton ref={myButton}>
-              <BsBellFill />
-            </IconButton>
+          <div className="Example">
+            <pre>
+              <code>
+                {`import { IconButton } from "abreactui"
+import { useRef } from "react"
+import { BsBellFill } from "react-icons/bs"
+
+export const IconButtonSample = () => {
+  const refButton = useRef(null)
+
+  return (
+    <IconButton
+      onMouseOut={() => {
+        refButton.current.style.color = ""
+      }}
+      onMouseOver={() => {
+        refButton.current.style.color = "yellowgreen"
+      }}
+      ref={refButton}
+    >
+      <BsBellFill />
+    </IconButton>
+  )
+}`}
+              </code>
+            </pre>
+            <div className="Result">
+              <IconButton
+                onMouseOut={() => {
+                  refButton.current.style.color = ""
+                }}
+                onMouseOver={() => {
+                  refButton.current.style.color = "yellowgreen"
+                }}
+                ref={refButton}
+              >
+                <BsBellFill />
+              </IconButton>
+            </div>
+          </div>
+        </article>
+        <article className="Sample">
+          <h2>
+            <code>style</code> props
+          </h2>
+          <div className="Example">
+            <pre>
+              <code>
+                {`import { IconButton } from "abreactui"
+import { BsBellFill, BsTrash3 } from "react-icons/bs"
+
+export const IconButtonSample = () => {
+  return (
+    <>
+      <IconButton style={{ color: "yellowgreen" }}>
+        <BsBellFill />
+      </IconButton>
+      <IconButton style={{ color: "red" }}>
+        <BsTrash3 />
+      </IconButton>
+    </>
+  )
+}`}
+              </code>
+            </pre>
+            <div className="Result">
+              <IconButton style={{ color: "yellowgreen" }}>
+                <BsBellFill />
+              </IconButton>
+              <IconButton style={{ color: "red" }}>
+                <BsTrash3 />
+              </IconButton>
+            </div>
+          </div>
+        </article>
+        <article className="Sample">
+          <h2>
+            <code>styled</code> props
+          </h2>
+          <div className="Example">
+            <pre>
+              <code>
+                {`import { Box, IconButton } from "abreactui"
+import { BsBellFill, BsTrash3 } from "react-icons/bs"
+
+export const IconButtonSample = () => {
+  return (
+    <>
+      <Box style={{ margin: "1rem" }}>
+        styled=&quot;fill&quot; button sample: &nbsp;
+        <IconButton styled="fill">
+          <BsBellFill />
+        </IconButton>
+        <IconButton styled="fill">
+          <BsTrash3 />
+        </IconButton>
+      </Box>
+      <Box style={{ margin: "1rem" }}>
+        styled=&quot;outline&quot; button sample: &nbsp;
+        <IconButton styled="outline">
+          <BsBellFill />
+        </IconButton>
+        <IconButton styled="outline">
+          <BsTrash3 />
+        </IconButton>
+      </Box>
+    </>
+  )
+}`}
+              </code>
+            </pre>
+            <div className="Result">
+              <Box style={{ margin: "1rem" }}>
+                styled=&quot;fill&quot; button sample: &nbsp;
+                <IconButton styled="fill">
+                  <BsBellFill />
+                </IconButton>
+                <IconButton styled="fill">
+                  <BsTrash3 />
+                </IconButton>
+              </Box>
+              <Box style={{ margin: "1rem" }}>
+                styled=&quot;outline&quot; button sample: &nbsp;
+                <IconButton styled="outline">
+                  <BsBellFill />
+                </IconButton>
+                <IconButton styled="outline">
+                  <BsTrash3 />
+                </IconButton>
+              </Box>
+            </div>
+          </div>
+          <div className="Example">
+            <pre>
+              <code>
+                {`import { Box, IconButton } from "abreactui"
+import { BsBellFill, BsBookmarkFill, BsGrid, BsTrash3 } from "react-icons/bs"
+
+export const IconButtonSample = () => {
+  return (
+    <Box style={{ margin: "1rem" }}>
+      Basic button and styled=&quot;normal&quot; button sample: &nbsp;
+      <IconButton>
+        <BsBellFill />
+      </IconButton>
+      <IconButton>
+        <BsTrash3 />
+      </IconButton>
+      <IconButton styled="normal">
+        <BsBookmarkFill />
+      </IconButton>
+      <IconButton styled="normal">
+        <BsGrid />
+      </IconButton>
+    </Box>
+  )
+}`}
+              </code>
+            </pre>
+            <div className="Result">
+              <Box style={{ margin: "1rem" }}>
+                Basic button and styled=&quot;normal&quot; button sample: &nbsp;
+                <IconButton>
+                  <BsBellFill />
+                </IconButton>
+                <IconButton>
+                  <BsTrash3 />
+                </IconButton>
+                <IconButton styled="normal">
+                  <BsBookmarkFill />
+                </IconButton>
+                <IconButton styled="normal">
+                  <BsGrid />
+                </IconButton>
+              </Box>
+            </div>
+          </div>
+        </article>
+        <article className="Sample">
+          <h2>
+            <code>styled disabled</code> props
+          </h2>
+          <div className="Example">
+            <pre>
+              <code>
+                {`import { IconButton } from "abreactui"
+import { BsBellFill, BsTrash3 } from "react-icons/bs"
+
+export const IconButtonSample = () => {
+  return (
+    <>
+      <IconButton disabled styled="fill">
+        <BsBellFill />
+      </IconButton>
+      <IconButton disabled styled="outline">
+        <BsTrash3 />
+      </IconButton>
+    </>
+  )
+}`}
+              </code>
+            </pre>
+            <div className="Result">
+              <IconButton disabled styled="fill">
+                <BsBellFill />
+              </IconButton>
+              <IconButton disabled styled="outline">
+                <BsTrash3 />
+              </IconButton>
+            </div>
+          </div>
+          <div className="Example">
+            <pre>
+              <code>
+                {`import { Box, IconButton } from "abreactui"
+import { BsBookmarkFill, BsGrid } from "react-icons/bs"
+
+export const IconButtonSample = () => {
+  return (
+    <>
+      <Box style={{ margin: "1rem" }}>
+        Disabled basic button button sample: &nbsp;
+        <IconButton disabled>
+          <BsBookmarkFill />
+        </IconButton>
+      </Box>
+      <Box style={{ margin: "1rem" }}>
+        Disabled styled=&quot;normal&quot; button sample: &nbsp;
+        <IconButton disabled styled="normal">
+          <BsGrid />
+        </IconButton>
+      </Box>
+    </>
+  )
+}`}
+              </code>
+            </pre>
+            <div className="Result">
+              <Box style={{ margin: "1rem" }}>
+                Disabled basic button button sample: &nbsp;
+                <IconButton disabled>
+                  <BsBookmarkFill />
+                </IconButton>
+              </Box>
+              <Box style={{ margin: "1rem" }}>
+                Disabled styled=&quot;normal&quot; button sample: &nbsp;
+                <IconButton disabled styled="normal">
+                  <BsGrid />
+                </IconButton>
+              </Box>
+            </div>
+          </div>
+        </article>
+        <article className="Sample">
+          <h2>
+            <code>tag</code> props
+          </h2>
+          <div className="Example">
+            <pre>
+              <code>
+                {`import { IconButton } from "abreactui"
+import { BsGoogle } from "react-icons/bs"
+
+export const IconButtonSample = () => {
+  return (
+    <IconButton href="https://google.com" tag="a" target="blank">
+      <BsGoogle />
+    </IconButton>
+  )
+}`}
+              </code>
+            </pre>
+            <div className="Result">
+              <IconButton href="https://google.com" tag="a" target="blank">
+                <BsGoogle />
+              </IconButton>
+            </div>
           </div>
         </article>
       </section>
