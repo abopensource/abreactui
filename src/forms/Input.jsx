0cdce303f03f8bf0c9e61d875c8b90505a427481
Aiden <bokjaeoh@gmail.com>
2024-11-26 17:43:09 +0900

    Update `Input` component and stylesheet module

--- src/forms/Input.jsx
+++ src/forms/Input.jsx
@@ -27,6 +27,7 @@ const Container = (props) => {
  * @param {String} [props.className] Stylesheet class name to apply to the user input field controller element to be created.
  * @param {String} [props.defaultValue] Default value of the user input field element.
  * @param {Boolean} [props.disabled] Whether the user input field element is disabled.
+ * @param {Boolean} [props.error] Whether the user input value is invalid.
  * @param {Boolean} [props.readOnly] Whether to use the user input field element as read-only.
  * @param {String} [props.styled="outline"] User input field style type.
  * "fill" | "outline"(default) | "underline"
@@ -35,7 +36,7 @@ const Container = (props) => {
  */
 const Controller = (props) => {
   const { defaultValue, disabled, readOnly, value, ...propsEF } = props
-  const { children, className, styled = "outline" } = propsEF
+  const { children, className, error, styled = "outline" } = propsEF
 
   const styles = [style.InputController]
   styles.push(style[styled[0].toUpperCase() + styled.slice(1)])
@@ -46,6 +47,7 @@ const Controller = (props) => {
 
   readOnly && styles.push(style.ReadOnly)
   disabled && styles.push(style.Disabled)
+  error && styles.push(style.Error)
   className && styles.push(className)
 
   return createElement({ children, props: { className: styles.join(" ") } })
@@ -78,6 +80,24 @@ const FieldSet = (props) => {
 }
 
 /**
+ * Returns after created the helper element(JSX element) for user input field element.
+ *
+ * @module Container
+ * @param {import("react").ComponentProps} props `React.ComponentProps` passed to React component.
+ * @param {import("react").ReactNode} props.children Child nodes to include in the helper element.
+ * @returns {import("react").ReactElement} Returns the created helper element(JSX element) for user input field element.
+ */
+const Helper = (props) => {
+  const { children } = props
+
+  return createElement({
+    children,
+    props: { className: style.Helper },
+    tag: "p",
+  })
+}
+
+/**
  * Returns after created the user input field element(JSX element).
  *
  * @module Field
@@ -87,6 +107,8 @@ const FieldSet = (props) => {
  * @see {@link https://developer.mozilla.org/docs/Web/HTML/Attributes/autocomplete}
  * @param {String} [props.defaultValue] Default value of the user input field element.
  * @param {Boolean} [props.disabled] Whether the user input field element is disabled.
+ * @param {Boolean} [props.error] Whether the user input value is invalid.
+ * @param {String} [props.helper] Help for user input values.
  * @param {String} [props.id] Identifier(`id`) attribute for the user input field element.
  * @param {EventListener} [props.onBlur] `EventListener` to execute when the user input field element has lost focus.
  * @param {EventListener} [props.onChange] `EventListener` to execute when the value of the user input field element changes.
@@ -94,14 +116,17 @@ const FieldSet = (props) => {
  * @param {String} [props.placeholder] Short hint that describes the expected value of the user input field element.
  * @param {Boolean} [props.readOnly] Whether to use the user input field element as read-only.
  * @param {Boolean} [props.required] Whether the user input field element is the required input field.
+ * @param {Function} [props.setError] Function that changes the error status of a user input field.
+ * @param {Function} [props.setHelper] Function that can set the help for a user input field.
  * @param {String} [props.type="text"] Field type of user input field element.
  * "password" | "search" | "text"(default)
  * @param {String} [props.value] Value of the user input field element.
  * @returns {import("react").ReactElement} Returns the created user input field element(JSX element).
  */
 const Field = (props) => {
-  const { onBlur, onChange, onFocus, ...propsOther } = props
-  const { defaultValue, type = "text", ...propsField } = propsOther
+  const { onBlur, onChange, onFocus, ...propsEH } = props
+  const { error, helper, setError, setHelper, ...propsEE } = propsEH
+  const { defaultValue, type = "text", ...propsField } = propsEE
 
   const [value, setValue] = React.useState(props.value || defaultValue || "")
   const [showPassword, setShowPassword] = React.useState(false)
@@ -134,6 +159,8 @@ const Field = (props) => {
     } else {
       eleCtrl?.classList?.remove(style.Filled)
     }
+
+    // TODO: Validation
 
     onChange && onChange(event)
   }
@@ -220,6 +247,8 @@ Field.propTypes = {
   autoComplete: PropTypes.string,
   defaultValue: PropTypes.string,
   disabled: PropTypes.bool,
+  error: PropTypes.bool,
+  helper: PropTypes.string,
   id: PropTypes.string,
   onBlur: PropTypes.func,
   onChange: PropTypes.func,
@@ -227,6 +256,8 @@ Field.propTypes = {
   placeholder: PropTypes.string,
   readOnly: PropTypes.bool,
   required: PropTypes.bool,
+  setError: PropTypes.func,
+  setHelper: PropTypes.func,
   type: PropTypes.oneOf(["password", "search", "text"]),
   value: PropTypes.string,
 }
@@ -262,6 +293,8 @@ const Label = (props) => {
  * @param {String} [props.className] Stylesheet class name to apply to the user input field controller element to be created.
  * @param {String} [props.defaultValue] Default value of the user input field element.
  * @param {Boolean} [props.disabled] Whether the user input field element is disabled.
+ * @param {Boolean} [props.error] Whether the user input value is invalid.
+ * @param {String} [props.helper] Help for user input values.
  * @param {String} [props.id] Identifier(`id`) attribute for the user input field element.
  * @param {String} [props.label] Label string for user input field element.
  * @param {EventListener} [props.onBlur] `EventListener` to execute when the user input field element has lost focus.
@@ -281,17 +314,25 @@ const Label = (props) => {
 const Input = React.forwardRef((props, forwardedRef) => {
   const { children, label, styled = "outline", ...propsInput } = props
 
+  const [error, setError] = React.useState(props.error || false)
+  const [helper, setHelper] = React.useState(props.helper || "")
+
   const childrenCtrl = []
   const childrenCont = []
 
   label && childrenCtrl.push(<Label id={props.id} key="L" label={label} />)
 
   forwardedRef && (propsInput.ref = forwardedRef)
+  propsInput.error = error
+  propsInput.setError = setError
+  propsInput.helper = helper
+  propsInput.setHelper = setHelper
   childrenCont.push(<Field key="I" {...propsInput} />)
 
   styled === "outline" && childrenCont.push(<FieldSet key="FS" label={label} />)
 
   childrenCtrl.push(<Container key="C">{childrenCont}</Container>)
+  helper && childrenCtrl.push(<Helper key="H">{helper}</Helper>)
 
   if (React.isValidElement(children)) {
     if (children.props?.position === "end") {
@@ -312,6 +353,8 @@ Input.propTypes = {
   className: PropTypes.string,
   defaultValue: PropTypes.string,
   disabled: PropTypes.bool,
+  error: PropTypes.bool,
+  helper: PropTypes.string,
   id: PropTypes.string,
   label: PropTypes.string,
   onBlur: PropTypes.func,
